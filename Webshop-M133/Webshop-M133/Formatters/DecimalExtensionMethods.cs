35f5c72c63327dac92c9d5962fe339265f0d94cd
imaan1411 <iman.luensmann@gmail.com>
2018-12-20 12:23:46 +0100

    project setup

---
+++ Webshop-M133/Webshop-M133/Formatters/DecimalExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace Dorflaedali.Formatters
+{
+    public static class DecimalExtensionMethods
+    {
+        public static string AsChf(this decimal input)
+        {
+            return input.ToString("CHF #.00", CultureInfo.InvariantCulture);
+        }
+    }
+}
